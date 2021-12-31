1f2441a386e5634d8ccdb4a759754b58b29a38dd
Felix Ding <felixding1989@gmail.com>
2021-12-30 21:12:50 -0800

    adds basic install bash script

---
+++ install.sh
@@ -0,0 +1,27 @@
+# mkdir -p ~/.config/nvim
+NEO_VIM_DIR_DEST="/Users/$USER/.config/nvim"
+NEO_VIM_DIR_SOURCE="./neovim_config"
+TMUX_DIR_DEST="/Users/$USER/.tmux.conf"
+TMUX_DIR_SOURCE="./tmux.conf"
+
+# determine OS
+[[ "$OSTYPE" == "linux-gnu"* ]] && OS="LINUX" || OS="NON-LINUX"
+
+# doing some actual work now
+if [ -d "$NEO_VIM_DIR_DEST" ]; then
+  echo "neovim config already exists"
+  echo "Please delete said directory if you wish to reconfigure neovim"
+else
+  echo "Symlinking neovim related folders..."
+  ln -s $NEO_VIM_DIR_SOURCE $NEO_VIM_DIR_DEST
+  echo "You would still need to run the plug installs manually"
+fi
+
+if [ -d "$TMUX_DIR_DEST" ]; then
+  echo "tmux config already exists"
+  echo "Please delete said config if you wish to reconfigure tmux"
+else 
+  echo "Symlinking tmux config..."
+  ln -s $TMUX_DIR_SOURCE $TMUX_DIR_DEST
+fi
+
