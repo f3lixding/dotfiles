70e563c5e0da4a26015481c0b70d634e7df299c8
Felix Ding <felixding1989@gmail.com>
2025-06-14 16:12:01 -0700

    updates for sketchybar

--- sketchybar/install_sketchybar.sh
+++ sketchybar/install_sketchybar.sh
@@ -4,7 +4,6 @@ brew install lua
 brew install switchaudio-osx
 brew install nowplaying-cli
 
-brew tap FelixKratz/formulae
 brew install sketchybar
 
 # Fonts
@@ -14,12 +13,11 @@ brew install --cask font-sf-pro
 
 curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.28/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
 
-# SbarLua
-(git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/)
+# SbarLua (enable this if we are using lua)
+# (git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/)
 
 echo "Cloning Config"
-git clone https://github.com/FelixKratz/dotfiles.git /tmp/dotfiles
+git clone https://github.com/f3lixding/sketchybar_conf.git /tmp/sketchybar
 mv $HOME/.config/sketchybar $HOME/.config/sketchybar_backup
-mv /tmp/dotfiles/.config/sketchybar $HOME/.config/sketchybar
-rm -rf /tmp/dotfiles
+mv /tmp/sketchybar $HOME/.config/sketchybar
 brew services restart sketchybar
