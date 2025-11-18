399d4bc86ff92ce3015c4a32aeca52448e1ff5c7
Felix Ding <felixding1989@gmail.com>
2025-11-17 21:00:32 -0800

    adds FHS env for spine

--- configuration.nix
+++ configuration.nix
@@ -43,6 +43,63 @@ let
       install -m755 zls $out/bin/zls
     '';
   };
+
+  spine-fhs = pkgs.buildFHSEnv {
+    name = "spine";
+    
+    targetPkgs = pkgs: (with pkgs; [
+      # Java
+      jdk17
+      
+      # X11 libraries - these provide the .so files
+      xorg.libX11
+      xorg.libXext
+      xorg.libXi
+      xorg.libXtst
+      xorg.libXrender
+      xorg.libXrandr
+      xorg.libXxf86vm
+      xorg.libXcursor
+      xorg.libXinerama
+      xorg.xdpyinfo
+      xorg.xrandr
+      
+      # OpenGL/Mesa
+      libGL
+      libGLU
+      mesa
+      
+      # Audio
+      alsa-lib
+      libpulseaudio
+      
+      # Other deps
+      freetype
+      fontconfig
+      zlib
+      glib
+      gtk3
+      cairo
+      pango
+      atk
+      gdk-pixbuf
+      
+      # C++ stdlib
+      stdenv.cc.cc.lib
+    ]);
+    
+    multiPkgs = pkgs: (with pkgs; [
+      # 32-bit libraries that might be needed
+      libGL
+      libGLU
+    ]);
+    
+    runScript = "bash -c 'cd ~/.cache/spine/Spine && ./Spine.sh'";
+    
+    profile = ''
+      export LD_LIBRARY_PATH=/usr/lib:/usr/lib32
+    '';
+  };
 in
 {
   imports =
@@ -50,6 +107,7 @@ in
       ./hardware-configuration.nix
     ];
 
+  # compatibility related
   nixpkgs.config.allowUnfree = true;
 
   # firmware update
@@ -134,6 +192,8 @@ in
 
     # media
     spotify
+    vesktop
+    wechat
 
     # hardware related
     usbutils
@@ -144,6 +204,9 @@ in
     # zig toolchains
     zig-0-15-1
     zls-0-15-0
+
+    # spine wrapper
+    spine-fhs
   ];
 
   # shell
