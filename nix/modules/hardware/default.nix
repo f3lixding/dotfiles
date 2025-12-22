790ac701d38b113ef7fc0992e0ac7a2b4f37e8cd
Felix Ding <felixding1989@gmail.com>
2025-12-22 01:15:09 -0800

    switches powersave to false for networkmanager

--- nix/modules/hardware/default.nix
+++ nix/modules/hardware/default.nix
@@ -24,6 +24,7 @@
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable =
     true; # Easiest to use and most distros use this by default.
+  networking.networkmanager.wifi.powersave = false;
 
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [
