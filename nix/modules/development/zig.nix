44dd14a2915376ff2f8200a469945ba42056c47e
Felix Ding <felixding1989@gmail.com>
2026-01-17 23:03:38 -0800

    upgrades zls

--- nix/modules/development/zig.nix
+++ nix/modules/development/zig.nix
@@ -1,4 +1,9 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   zig-latest = pkgs.stdenv.mkDerivation {
@@ -6,8 +11,7 @@ let
     version = "0.15.2";
 
     src = builtins.fetchTarball {
-      url =
-        "https://ziglang.org/download/0.15.2/zig-x86_64-linux-0.15.2.tar.xz";
+      url = "https://ziglang.org/download/0.15.2/zig-x86_64-linux-0.15.2.tar.xz";
       sha256 = "0skmy2qjg2z4bsxnkdzqp1hjzwwgnvqhw4qjfnsdpv6qm23p4wm0";
     };
 
@@ -26,8 +30,8 @@ let
     version = "0.15.0";
 
     src = builtins.fetchTarball {
-      url = "https://builds.zigtools.org/zls-x86_64-linux-0.15.0.tar.xz";
-      sha256 = "1jdl9sa8f1nxy6j7jydr1g0ixw4rf6b9myalnljhy9xwwi1d3lhs";
+      url = "https://github.com/zigtools/zls/releases/download/0.15.1/zls-x86_64-linux.tar.xz";
+      sha256 = "1w00vdf0yb3q7avpdzgwifjvld4bx4snfbj27y02ha9br5lw6ibd";
     };
 
     sourceRoot = ".";
@@ -40,4 +44,10 @@ let
       install -m755 $src/zls $out/bin/zls
     '';
   };
-in { environment.systemPackages = [ zig-latest zls-latest ]; }
+in
+{
+  environment.systemPackages = [
+    zig-latest
+    zls-latest
+  ];
+}
